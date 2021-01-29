6b296c6e31dbcbbdaf70b8cc46c97699be798165
yuanbao97 <316261393@qq.com>
2021-01-29 10:24:46 +0800

    init

---
+++ app/src/main/java/com/example/coolweather/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.coolweather.activity;
+
+import android.os.Bundle;
+
+import com.example.coolweather.R;
+import com.example.coolweather.framework.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
